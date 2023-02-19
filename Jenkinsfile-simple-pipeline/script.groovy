4e19b47c20ed9ba3b3e4ee11edeca252c42e3a44
muhab404 <83089239+muhab404@users.noreply.github.com>
2023-02-19 18:42:53 +0200

    Update script.groovy

--- Jenkinsfile-simple-pipeline/script.groovy
+++ Jenkinsfile-simple-pipeline/script.groovy
@@ -4,7 +4,7 @@ def buildJar() {
 } 
 
 def buildImage() {
-    echo "building the docker image....."
+    echo "building the docker image......"
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t muhab404/jenkins-demo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
