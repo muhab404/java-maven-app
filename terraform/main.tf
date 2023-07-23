bb6205be48336d42eba46bd309acc9b473fd172d
muhab404 <83089239+muhab404@users.noreply.github.com>
2023-07-23 10:45:40 +0300

    fix terraform key name 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,3 +111,22 @@ resource "aws_instance" "myapp-server_2" {
 output "ec2_public_ip" {
     value = aws_instance.myapp-server_2.public_ip
 }
+
+data "aws_key_pair" "example" {
+  key_name           = "server-key"
+  include_public_key = true
+
+
+}
+
+output "fingerprint" {
+  value = data.aws_key_pair.example.fingerprint
+}
+
+output "name" {
+  value = data.aws_key_pair.example.key_name
+}
+
+output "id" {
+  value = data.aws_key_pair.example.id
+}
