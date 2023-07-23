52ca24bcfd7f2d3d864ebc3158e13aa55155864d
muhab404 <83089239+muhab404@users.noreply.github.com>
2023-07-23 10:39:19 +0300

    fix terraform key name

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,17 +89,17 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-resource "aws_instance" "myapp-server" {
+resource "aws_instance" "myapp-server_2" {
     # ami = data.aws_ami.latest-amazon-linux-image.id
     ami = "ami-053b0d53c279acc90"
     instance_type = var.instance_type
+    key_name = "server-key"
 
     subnet_id = aws_subnet.myapp-subnet-1.id
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
-    key_name = "server-key"
 
     user_data = file("entry-script.sh")
 
@@ -109,5 +109,5 @@ resource "aws_instance" "myapp-server" {
 }
 
 output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
+    value = aws_instance.myapp-server_2.public_ip
 }
